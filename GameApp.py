ca49df0eeea80beafcb4d905f7481bdc105f2a48
Denys Lykov <lykov.denis@gmail.com>
2023-05-25 14:32:56 +0300

    Adding mike image and collision detection with raindrops

--- GameApp.py
+++ GameApp.py
@@ -1,32 +1,57 @@
-import pygame, sys, random
+import pygame, sys, random, time
 from pygame.locals import *
 
 pygame.init()
 pygame.display.set_caption("First Program")
 
 mike_umbrella_image = pygame.image.load("images/umbrella.png")
+mike_image = pygame.image.load("images/mike.png")
+cloud_image = pygame.image.load("images/cloud.png")
 
-screen = pygame.display.set_mode((1000,600))
+screen = pygame.display.set_mode((1300,700))
 
 clock = pygame.time.Clock()
 
+last_hit_time = 0
+
 raindrop_spawn_time = 0
+
+class Cloud:
+    def __init__(self):
+        self.x = 500
+        self.y = 0
+
+    def rain(self):
+        for i in range(10):
+            raindrops.append(Raindrop(random.randint(self.x+75, self.x+300), self.y+150))
+
+    def draw(self):
+        screen.blit(cloud_image, (self.x, self.y))
+
+    def move(self):
+        if pressed_keys[K_RIGHT]:        
+            self.x += 1
+        if pressed_keys[K_LEFT]:        
+            self.x -= 1
 
 class Mike:
     def __init__(self):
-        self.x = 300
-        self.y = 100
+        self.x = 600
+        self.y = 350
 
     def draw(self):
-        screen.blit(mike_umbrella_image, (self.x, self.y))
+        if time.time() > last_hit_time + 1:
+            screen.blit(mike_image, (self.x, self.y))
+        else:
+            screen.blit(mike_umbrella_image, (self.x, self.y))
 
     def hit_by(self, raindrop):
-        return pygame.Rect(self.x, self.y, 170, 300).collidepoint((raindrop.x, raindrop.y))
+        return pygame.Rect(self.x, self.y, 250, 300).collidepoint((raindrop.x, raindrop.y))
 
 class Raindrop:
-    def __init__(self):
-        self.x = random.randint(0, 1000)
-        self.y = -8
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
         self.speed = random.randint(5, 18)
 
     def move(self):
@@ -34,30 +59,38 @@ class Raindrop:
 
     def draw(self):
         pygame.draw.line(screen, (0,0,0), (self.x, self.y), (self.x, self.y+8), 1)
+
     def off_screen(self):
         return self.y > 800
 
 raindrops = []
 mike = Mike()
+cloud = Cloud()
 
 while 1:
     clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        raindrops.append(Raindrop())
-        #pressed_keys = pygame.key.get_pressed()
-        #if pressed_keys[K_RIGHT]:        
-        #    xpos += 1
-        #if pressed_keys[K_LEFT]:        
-        #    xpos -= 1
+        
+        pressed_keys = pygame.key.get_pressed()
+
         screen.fill((255,255,255))
         mike.draw()
+        cloud.draw()
+        cloud.rain()
+        cloud.move()
         i = 0
         while i < len(raindrops):
             raindrops[i].move()
             raindrops[i].draw()
-            if raindrops[i].off_screen() or mike.hit_by(raindrops[i]):
+            flag = False
+            if raindrops[i].off_screen():
+                flag = True
+            if mike.hit_by(raindrops[i]):
+                flag = True
+                last_hit_time = time.time()
+            if flag:
                 del raindrops[i]
                 i -= 1
             i += 1
